b4831c67d7e087492c6798c4b3f4ee42433e5390
Syed Meeran Ishrat Hussain <Hussain-SMI@ulster.ac.uk>
2024-10-08 10:23:14 +0100

    initial commit 3.1 - pg no added

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+    int NoOfPages;
+
+
+
+    void DisplayInfo()
+    {
+        // Output the book information to the console
+        Console.WriteLine("Book information");
+        Console.WriteLine("-----------------");
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+        Console.WriteLine($"Book.No Of Pages: {NoOfPages}");
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        // Create a new instance of the book class
+
+        Book book = new Book();
+        book.Title = "C# for beginners";
+        book.Author = "Bill Gates";
+        book.ISBN = "12345678";
+        book.NoOfPages = 200;
+
+        Book book2 = new Book();
+        book2.Title = "C# methods and classes";
+        book2.Author = "Microsoft";
+        book2.ISBN = "1234455";
+        book2.NoOfPages = 50;
+
+        // output book info to console
+        book.DisplayInfo();
+        book2.DisplayInfo();
+
+
+    }    
+}
